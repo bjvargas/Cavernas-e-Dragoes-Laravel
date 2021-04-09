5c5751c42e1dfdf26e822752161dfd2d70c04aa7
Lucas Cunha <lucasdcunha42@gmail.com>
2021-04-09 00:23:46 -0300

    Commit dark-Table - Fim Video 4 = ) =

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,21 +5,21 @@
 
     <div class="text-center mt-3 mb-4">    
     <a ref="">
-              <button class="bnt bnt-dark"> Visualizar </button>
+              <button class="btn btn-success"> Cadastrar </button>
           </a>       
     </div> 
 
     <div class="col-8 m-auto">
-    <table class="table text-center">
-  <thead>
-    <tr>
-      <th scope="col">Id Personagem</th>
-      <th scope="col">Nome</th>
-      <th scope="col">Jogador</th>
-      <th scope="col">Quantidade Personagens</th>
-    </tr>
-  </thead>
-  <tbody>
+       <table class="table text-center">
+         <thead class="table-dark">
+          <tr>
+            <th scope="col">Id Personagem</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Jogador</th>
+            <th scope="col">Quantidade Personagens</th>
+          </tr>
+        </thead>
+        <tbody>
 
     @foreach($listaPersonagens as $personagem)
         @php
@@ -31,15 +31,15 @@
       <td>{{$user->name}}</td>
       <td>
           <a ref=""> 
-           <button class="bnt bnt-dark"> Visualizar </button>
+           <button class="btn btn-dark"> Visualizar </button>
           </a>      
           
           <a ref="">
-           <button class="bnt bnt-primary"> Editar </button>
+           <button class="btn btn-primary"> Editar </button>
           </a>      
 
           <a ref="">
-           <button class="bnt bnt-danger"> Deletar </button>
+           <button class="btn btn-danger"> Deletar </button>
           </a>      
       </td>
     </tr>
