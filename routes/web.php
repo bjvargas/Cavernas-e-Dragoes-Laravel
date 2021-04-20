1a6933bdcb3aa16fd27f072f71a23cb9ed69c617
Bruno Vargas <bruno_jaques@hotmail.com>
2021-04-19 22:40:24 -0300

    Login de user

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PersonagemController;
 use App\Http\Controllers\Teste;
 
-Route::resource('/personagens', PersonagemController::class);
-Route::resource('/teste', Teste::class);
+Route::resource('/personagens', PersonagemController::class)->middleware('auth');
 
+Auth::routes();
+Route::get('/home', 'HomeController@index');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
