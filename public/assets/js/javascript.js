e2ba95c9917bbd111d42f6aca711687ecb561cb9
Bruno Jaques Olivera de Vargas <70863493+bjvargas@users.noreply.github.com>
2021-04-27 01:41:20 -0300

    ajustando erro js

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,136 +1,126 @@
-(function(win,doc){
+(function(win, doc) {
     'use strict';
 
     //delete personagens
-    function confirmDel(event)
-    {
+    function confirmDel(event) {
         event.preventDefault()
-        let token=doc.getElementsByName("_token")[0].value;
-        if(confirm("Eu não fui um bom Personagem? T_T, ou você me matou?")){
-                let ajax =new XMLHttpRequest();
-                ajax.open('DELETE',event.target.parentNode.href);
-                ajax.setRequestHeader('X-CSRF-TOKEN',token);
-                ajax.onreadystatechange=function(){
-                    if(ajax.readyState === 4 && ajax.status === 200){
-                    win.location.href="personagens";
-                    }
-                };
-                ajax.send();
-        }else{
+        let token = doc.getElementsByName("_token")[0].value;
+        if (confirm("Eu não fui um bom Personagem? T_T, ou você me matou?")) {
+            let ajax = new XMLHttpRequest();
+            ajax.open('DELETE', event.target.parentNode.href);
+            ajax.setRequestHeader('X-CSRF-TOKEN', token);
+            ajax.onreadystatechange = function() {
+                if (ajax.readyState === 4 && ajax.status === 200) {
+                    win.location.href = "personagens";
+                }
+            };
+            ajax.send();
+        } else {
             return false;
         }
 
     }
-    if(doc.querySelector('.js-del')){
-        let btn=doc.querySelectorAll('.js-del');
-        for(let i =0; i<btn.length; i++){
-            btn[i].addEventListener('click',confirmDel,false);
+    if (doc.querySelector('.js-del')) {
+        let btn = doc.querySelectorAll('.js-del');
+        for (let i = 0; i < btn.length; i++) {
+            btn[i].addEventListener('click', confirmDel, false);
         }
     }
 
 
-})(window,document);
+})(window, document);
 
-(function(win,doc){
+(function(win, doc) {
     'use strict';
 
-   //delete magias
-   function confirmDelM(event)
-   {
-       event.preventDefault()
-       let token=doc.getElementsByName("_token")[0].value;
-       if(confirm("Ficou sem mana?")){
-               let ajax =new XMLHttpRequest();
-               ajax.open('DELETE',event.target.parentNode.href);
-               ajax.setRequestHeader('X-CSRF-TOKEN',token);
-               ajax.onreadystatechange=function(){
-                   if(ajax.readyState === 4 && ajax.status === 200){
-                   win.location.href="magias";
-                   }
-               };
-               ajax.send();
-       }else{
-           return false;
-       }
+    //delete magias
+    function confirmDelM(event) {
+        event.preventDefault()
+        let token = doc.getElementsByName("_token")[0].value;
+        if (confirm("Ficou sem mana?")) {
+            let ajax = new XMLHttpRequest();
+            ajax.open('DELETE', event.target.parentNode.href);
+            ajax.setRequestHeader('X-CSRF-TOKEN', token);
+            ajax.onreadystatechange = function() {
+                if (ajax.readyState === 4 && ajax.status === 200) {
+                    win.location.href = "magias";
+                }
+            };
+            ajax.send();
+        } else {
+            return false;
+        }
 
-   }
-   if(doc.querySelector('.js-deu')){
-       let btn=doc.querySelectorAll('.js-deu');
-       for(let i =0; i<btn.length; i++){
-           btn[i].addEventListener('click',confirmDelM,false);
-       }
-   }
+    }
+    if (doc.querySelector('.js-deu')) {
+        let btn = doc.querySelectorAll('.js-deu');
+        for (let i = 0; i < btn.length; i++) {
+            btn[i].addEventListener('click', confirmDelM, false);
+        }
+    }
 
 
-})(window,document);
+})(window, document);
 
-(function(win,doc){
+(function(win, doc) {
     'use strict';
 
 
- //deleta equipamento
- function confirmDelE(event)
- {
-     event.preventDefault()
-     let token=doc.getElementsByName("_token")[0].value;
-     if(confirm("Quebrou o que não devia?")){
-             let ajax =new XMLHttpRequest();
-             ajax.open('DELETE',event.target.parentNode.href);
-             ajax.setRequestHeader('X-CSRF-TOKEN',token);
-             ajax.onreadystatechange=function(){
-                 if(ajax.readyState === 4 && ajax.status === 200){
-                 win.location.reload();
-                 }
-             };
-             ajax.send();
-     }else{
-         return false;
-     }
+    //deleta equipamento
+    function confirmDelE(event) {
+        event.preventDefault()
+        let token = doc.getElementsByName("_token")[0].value;
+        if (confirm("Quebrou o que não devia?")) {
+            let ajax = new XMLHttpRequest();
+            ajax.open('DELETE', event.target.parentNode.href);
+            ajax.setRequestHeader('X-CSRF-TOKEN', token);
+            ajax.onreadystatechange = function() {
+                if (ajax.readyState === 4 && ajax.status === 200) {
+                    win.location.reload();
+                }
+            };
+            ajax.send();
+        } else {
+            return false;
+        }
 
- }
- if(doc.querySelector('.js-delEquip')){
-     let btn=doc.querySelectorAll('.js-delEquip');
-     for(let i =0; i<btn.length; i++){
-         btn[i].addEventListener('click',confirmDelE,false);
-     }
- }
+    }
+    if (doc.querySelector('.js-delEquip')) {
+        let btn = doc.querySelectorAll('.js-delEquip');
+        for (let i = 0; i < btn.length; i++) {
+            btn[i].addEventListener('click', confirmDelE, false);
+        }
+    }
 
-})(window,document);
+})(window, document);
+(function(win, doc) {
+    'use strict';
 
-   //delete Lista magias
-   function confirmDelMP(event)
-   {
-       event.preventDefault()
-       let token=doc.getElementsByName("_token")[0].value;
-       if(confirm("Perdestes o grimório?")){
-               let ajax =new XMLHttpRequest();
-               ajax.open('DELETE',event.target.parentNode.href);
-               ajax.setRequestHeader('X-CSRF-TOKEN',token);
-               ajax.onreadystatechange=function(){
-                   if(ajax.readyState === 4 && ajax.status === 200){
+    //delete Lista magias
+    function confirmDelMP(event) {
+        event.preventDefault()
+        let token = doc.getElementsByName("_token")[0].value;
+        if (confirm("Perdestes o grimório?")) {
+            let ajax = new XMLHttpRequest();
+            ajax.open('DELETE', event.target.parentNode.href);
+            ajax.setRequestHeader('X-CSRF-TOKEN', token);
+            ajax.onreadystatechange = function() {
+                if (ajax.readyState === 4 && ajax.status === 200) {
                     window.location.reload();
-                   }
-               };
-               ajax.send();
-       }else{
-           return false;
-       }
+                }
+            };
+            ajax.send();
+        } else {
+            return false;
+        }
 
-   }
-   if(doc.querySelector('.js-delMP')){
-       let btn=doc.querySelectorAll('.js-delMP');
-       for(let i =0; i<btn.length; i++){
-           btn[i].addEventListener('click',confirmDelMP,false);
-       }
-   }
-
-
-})(window,document);
+    }
+    if (doc.querySelector('.js-delMP')) {
+        let btn = doc.querySelectorAll('.js-delMP');
+        for (let i = 0; i < btn.length; i++) {
+            btn[i].addEventListener('click', confirmDelMP, false);
+        }
+    }
 
 
-
-
-
-
-
-    
+})(window, document);
