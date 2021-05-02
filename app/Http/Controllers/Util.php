e45f0587e53e8452c5f48456b64393c88c3f8619
Lucas Cunha <lucasdcunha42@gmail.com>
2021-05-02 16:27:57 -0300

    Apagando Comentarios da Util

--- app/Http/Controllers/Util.php
+++ app/Http/Controllers/Util.php
@@ -6,21 +6,13 @@ use Illuminate\Http\Request;
 
 class Util extends Controller
 {
-    /**
-     * Método de conversão de status base de atributo para modificador.
-     * int $valor: atributo base.
-     * return: Modificador
-     */
+ 
     public function converteAtributo($valor)
     {
         return floor(($valor - 10) / 2);
     }
 
-    /**
-     * Calcula hp inicial do char.
-     * integer $hp e int $modConstituicao .
-     * return: Modificador
-     */
+
     public function calculaHpInicial($dadoVida, $modConstituicao)
     {
         
