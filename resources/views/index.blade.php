3ea7113b6e6d18778aad28cef666e45e9fa78cf5
Bruno Jaques Olivera de Vargas <amanda.jaques.vargas@outlook.com>
2021-06-12 05:16:47 -0300

    Ajustes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,14 @@
   <meta charset="UTF-8">
   <title>Bem vindo aventureiro!</title>
   <link rel="stylesheet" type="text/css" href="{{ url('/css/index.css') }}" />
+  <link rel="stylesheet" type="text/css" href="{{ url('/css/footer.css') }}" />
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 </head>
 
 <body>
 
-  <nav>
+  <header class="cabecalho">
     <ul class="menu">
       <li><a href="#">Home</a></li>
       <li><a href="#">Classes</a></li>
@@ -18,34 +21,44 @@
       <li><a href="#">Antecedentes</a></li>
       <li><a href="#">Magias</a></li>
       <li><a href="#">Equipamentos</a></li>
+      <li><a href="#">Login</a></li>
 
     </ul>
-  </nav>
 
-  <section class="sections">
+  </header>
 
-    <section class="bloco" id="home">
-      <div class="container">
-
-        <div class="fadeIn">
-          <img src="/imagens/logolowqualityblack.webp">
-        </div>
-
-        <div class="fadeIn2">
-          <p>Crie seus personagens consultando informações como raças, classes, magias, talentos e antecedentes.
-        Poderá evoluir, adicionar equipamentos, magias e muito mais.</p>
-        </div>
-
-
+  <section class="conteudo">
+    <div class="container">
+      <div class="fadeIn1">
+        <p>Precisando de um lugar para criar e armazenar seus personagens em D&D 5e?</p>
       </div>
 
-    </section>
+      <div class="fadeIn2">
+        <p>Crie seus personagens consultando informações como raças, classes, magias, talentos e antecedentes.
+          Poderá evoluir, adicionar equipamentos, magias e muito mais.</p>
+      </div>
 
-   
+      <div class="fadeIn3">
+        <p>Seja muito bem vindo aventureiro!</p>
+      </div>
 
-    <footer>
-      <p>Todos os direitos reservados Copyright&COPY; 2021</p>
-    </footer>
+      <div class="fadeIn4">
+        <img src="/imagens/logolowqualityblack.webp">
+      </div>
+
+    </div>
+  </section>
+
+  <footer class="rodape">
+    <div class="d-flex justify-content-end social_icon">
+      <p class="textos-footer">Nos siga nas redes sociais</p>
+      <span><i class="fab fa-facebook-square"></i></span>
+      <span><i class="fab fa-google-plus-square"></i></span>
+      <span><i class="fab fa-twitter-square"></i></span>
+    </div>
+    <div class="textofinal-footer">Todos os direitos reservados Copyright &COPY; 2021</div>
+  </footer>
+
 </body>
 
 </html>
