4af7e317f2ffbc8fcb545927d3d23f389b26a4a2
Lucas Cunha <lucasdcunha42@gmail.com>
2021-06-12 23:17:40 -0300

    Alt seeder personagem Vida

--- database/seeders/PersonagemSeeder.php
+++ database/seeders/PersonagemSeeder.php
@@ -5,9 +5,20 @@ namespace Database\Seeders;
 use App\Models\Personagem;
 use Illuminate\Database\Seeder;
 use App\Http\Controllers\Util;
+use App\Models\Classes;
 
 class PersonagemSeeder extends Seeder
 {
+    
+    private $objClasse;
+    private $objUtil;
+            
+    public function __construct()
+    {
+        $this->objClasse=new Classes();
+        $this->objUtil=new Util();
+    }
+
         /**
      * Run the database seeds.
      *
@@ -15,108 +26,165 @@ class PersonagemSeeder extends Seeder
      */
     public function run()
     {
-        Personagem::create(['nome'=>'Anaxibia', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);        
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Anaxibia', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20),
+         'vida' => $this->atribuirHp($constituicao, $idClasse)]);        
 
-        Personagem::create(['nome'=>'Bromios', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Bromios', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Cybele', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Cybele', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
         
-        Personagem::create(['nome'=>'Eidothea', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Eidothea', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Fawkes', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Fawkes', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Galateia', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Galateia', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Eustacia', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Eustacia', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Hopladamos', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Hopladamos', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Iphis', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Iphis', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Justitia', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Justitia', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Kaunos', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Kaunos', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Lampetos', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Lampetos', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Menestheus', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Menestheus', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Nyx', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Nyx', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Orthaia', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Orthaia', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Perseus', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Perseus', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Quyou', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Quyou', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Rhadamanthys', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Rhadamanthys', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Silvanus', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Silvanus', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Thamyris', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Thamyris', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Uranos', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Uranos', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Vesta', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Vesta', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Wynefreede', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Wynefreede', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Xuxa', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Xuxa', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Ywain', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Ywain', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
 
-        Personagem::create(['nome'=>'Zephyrus', 'id_classe' => rand(1,12), 'id_raca'=>rand(1,10), 'id_user'=>'1', 
-        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> rand(6,20), 'inteligencia'=> rand(6,20), 
-        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => rand(10,20)]);
+        $constituicao=rand(6,20);
+        $idClasse=rand(1,12);
+        Personagem::create(['nome'=>'Zephyrus', 'id_classe' => $idClasse, 'id_raca'=>rand(1,10), 'id_user'=>'1', 
+        'forca' => rand(6,20), 'destreza'=> rand(6,20), 'constituicao'=> $constituicao, 'inteligencia'=> rand(6,20), 
+        'sabedoria'=> rand(6,20), 'carisma'=> rand(6,20), 'vida' => $this->atribuirHp($constituicao, $idClasse)]);
+    }
+    public function atribuirHp($constituicao,$idClasse){
+        $classe=$this->objClasse->find($idClasse);
+        return $this->objUtil->calculaHpInicial($classe->dado_vida, $this->objUtil->converteAtributo($constituicao));        
     }
 }
